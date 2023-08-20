49ca21d6de842ba270658482bc199dc578fe5f8a
Rishi Kumar Singh <112893296+rishi2806@users.noreply.github.com>
2023-08-20 22:19:12 +0530

    Add files via upload

---
+++ qr_code/script.js
@@ -0,0 +1,38 @@
+const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrContainer = document.querySelector('.qr-body');
+
+let size = sizes.value;
+generateBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    isEmptyInput();
+});
+
+
+downloadBtn.addEventListener('click', ()=>{
+    let img = document.querySelector('.qr-body img');
+
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
+
+function isEmptyInput(){
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter valid url");;
+}
+function generateQRCode(){
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        height:size,
+        width:size,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
